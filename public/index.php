6aa156159ab3cc71a9b061f772090a96d321b6cd
Joncart yoann <m.j.yoann@gmail.com>
2025-05-06 12:04:23 +0200

    test creation page acceuil

--- public/index.php
+++ public/index.php
@@ -2,9 +2,26 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <
+        <title>Acceuil</title>
+</head>
+
+<body>
+    <button type="button" class="btn btn-primary">Primary</button>
+</body>
+
+</html>
+
